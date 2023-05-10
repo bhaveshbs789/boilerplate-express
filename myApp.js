724230e1a9b405978586b1ff6ef5beb937a298ac
Bhavesh Sharma <bhaveshbs789@gmail.com>
2023-05-10 14:12:12 +0530

    Adding middleware code

--- myApp.js
+++ myApp.js
@@ -1,9 +1,13 @@
+require('dotenv').config()
 let express = require('express');
 let app = express();
 
-// console.log("Hello World");
+app.use(express.static(__dirname + "/public"));
 
-app.use("/public", express.static(__dirname + "/public"));
+app.use(function(req, res, next){
+    console.log(`${req.method} ${req.path} - ${req.ip}`);
+    next();
+});
 
 app.get("/", function(req, res) {
     // res.send("Hello Express");
@@ -12,7 +16,7 @@ app.get("/", function(req, res) {
 
 app.get("/json", function(req, res) {
     res.json({
-        "message":"Hello json"
+        "message": process.env.MESSAGE_STYLE == "uppercase" ? "HELLO JSON" : "Hello json"
     })
 })
 
