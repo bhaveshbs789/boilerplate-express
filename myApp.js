a0257c7614ac513a249c6e9c8ed761fe0431fdcb
Bhavesh Sharma <bhaveshbs789@gmail.com>
2023-05-10 12:31:36 +0530

    Added root route

--- myApp.js
+++ myApp.js
@@ -1,7 +1,22 @@
 let express = require('express');
 let app = express();
 
-console.log("Hello World");
+// console.log("Hello World");
+
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/", function(req, res) {
+    // res.send("Hello Express");
+    res.sendFile(__dirname + "/views/index.html");
+})
+
+app.get("/json", function(req, res) {
+    res.json({
+        "message":"Hello json"
+    })
+})
+
+
 
 
 
